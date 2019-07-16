4295115bd7ba3ae8ea6069d3b876e58db2abb88f
StevenSymons <symons_steven@hotmail.com>
2019-07-16 22:51:08 +0200

    added new routes

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,15 @@ import React from "react";
 import { Navbar } from "./Navbar";
 import { Content } from "./Content/Content";
 import { Footer } from "./Footer/Footer";
+import ProductDetails from "./Content/ProductDetails/ProductDetails";
+import WishList from "./Content/WishList/WishList";
 import styled from "styled-components";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 
 const AppWrapper = styled.div`
   display: flex;
@@ -12,11 +20,18 @@ const AppWrapper = styled.div`
 
 const App = () => {
   return (
-    <AppWrapper>
-      <Navbar />
-      <Content />
-      <Footer />
-    </AppWrapper>
+    <Router>
+      <AppWrapper>
+        <Navbar />
+        <Switch>
+          <Route exact path="/dames" component={Content} />
+          <Route path="/dames/:id" component={ProductDetails} />
+          <Route path="/wish-list" component={WishList} />
+          <Redirect exact from="/" to="/dames" />
+        </Switch>
+        <Footer />
+      </AppWrapper>
+    </Router>
   );
 };
 
